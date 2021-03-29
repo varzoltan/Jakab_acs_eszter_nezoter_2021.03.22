c8890010e872f1bb25a2fc954d261018c6499d5c
varzoltan <varzoltan@tanarium.hu>
2021-03-29 15:00:09 +0200

    14:59

--- Jakab_acs_eszter_nezoter_2021.03.22/Program.cs
+++ Jakab_acs_eszter_nezoter_2021.03.22/Program.cs
@@ -23,7 +23,7 @@ namespace Jakab_acs_eszter_nezoter_2021._03._22
         {
             Adat[] adatok = new Adat[15];
             int n = 0;
-            StreamReader beolvas = new StreamReader(@"E:\OneDrive - Kisvárdai SZC Móricz Zsigmond Szakgimnáziuma és Szakközépiskolája\Oktatas\Programozas\Jakab_Acs_Eszter\Erettsegi_feladatok\2014-oktober_uj\foglaltsag.txt");
+            StreamReader beolvas = new StreamReader(@"C:\Users\Rendszergazda\Desktop\J_Eszter_prog_feladat\foglaltsag.txt");
             while (!beolvas.EndOfStream)
             {
                 string sor = beolvas.ReadLine();
@@ -33,7 +33,7 @@ namespace Jakab_acs_eszter_nezoter_2021._03._22
             beolvas.Close();
 
             n = 0;
-            StreamReader beolvas2 = new StreamReader(@"E:\OneDrive - Kisvárdai SZC Móricz Zsigmond Szakgimnáziuma és Szakközépiskolája\Oktatas\Programozas\Jakab_Acs_Eszter\Erettsegi_feladatok\2014-oktober_uj\kategoria.txt");
+            StreamReader beolvas2 = new StreamReader(@"C:\Users\Rendszergazda\Desktop\J_Eszter_prog_feladat\kategoria.txt");
             while (!beolvas2.EndOfStream)
             {
                 string sor = beolvas2.ReadLine();
@@ -48,13 +48,35 @@ namespace Jakab_acs_eszter_nezoter_2021._03._22
             int sorszam = int.Parse(Console.ReadLine());
             Console.Write("Kérem adja meg a szék számát: ");
             int szekszam = int.Parse(Console.ReadLine());
-            for (int i = 0; i<=sorszam; i++)
+            for (int i = 0; i<n; i++)
             {
-                if (i+1 == sorszam)
+                if (i == sorszam-1)
                 {
-                    
+                    if(adatok[i].foglaltsag.Substring(szekszam-1,1) == "x")
+                    {
+                        Console.WriteLine("A hely nem szabad.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("A hely még szabad.");
+                    }
                 }
             }
+
+            //3.feladat
+            const int nezoszam = 15 * 20;
+            int szamlalo = 0;
+            for (int i=0; i<n; i++)
+            {
+                for (int k=0; k<adatok[i].foglaltsag.Length;k++)
+                {
+                    if (adatok[i].foglaltsag[k] == 'x')
+                    {
+                        szamlalo++;
+                    }  
+                }
+            }
+            Console.WriteLine($"Az előadásra eddig {szamlalo} jegyet adtak el, ez a nézőtér {szamlalo  /  n * 100}");
             Console.ReadKey();
         }
     }
