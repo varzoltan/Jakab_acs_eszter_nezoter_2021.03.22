1802e7f60853f7683bd40cc34093c00712eaeac8
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-04-05 12:16:43 +0200

    12.16

--- Jakab_acs_eszter_nezoter_2021.03.22/Program.cs
+++ Jakab_acs_eszter_nezoter_2021.03.22/Program.cs
@@ -23,7 +23,7 @@ namespace Jakab_acs_eszter_nezoter_2021._03._22
         {
             Adat[] adatok = new Adat[15];
             int n = 0;
-            StreamReader beolvas = new StreamReader(@"C:\Users\Rendszergazda\Desktop\J_Eszter_prog_feladat\foglaltsag.txt");
+            StreamReader beolvas = new StreamReader(@"E:\OneDrive - Kisvárdai SZC Móricz Zsigmond Szakgimnáziuma és Szakközépiskolája\Oktatas\Programozas\Jakab_Acs_Eszter\Erettsegi_feladatok\2014-oktober_uj\foglaltsag.txt");
             while (!beolvas.EndOfStream)
             {
                 string sor = beolvas.ReadLine();
@@ -33,7 +33,7 @@ namespace Jakab_acs_eszter_nezoter_2021._03._22
             beolvas.Close();
 
             n = 0;
-            StreamReader beolvas2 = new StreamReader(@"C:\Users\Rendszergazda\Desktop\J_Eszter_prog_feladat\kategoria.txt");
+            StreamReader beolvas2 = new StreamReader(@"E:\OneDrive - Kisvárdai SZC Móricz Zsigmond Szakgimnáziuma és Szakközépiskolája\Oktatas\Programozas\Jakab_Acs_Eszter\Erettsegi_feladatok\2014-oktober_uj\kategoria.txt");
             while (!beolvas2.EndOfStream)
             {
                 string sor = beolvas2.ReadLine();
@@ -65,6 +65,7 @@ namespace Jakab_acs_eszter_nezoter_2021._03._22
 
             //3.feladat
             const int nezoszam = 15 * 20;
+            //Console.WriteLine(nezoszam);
             int szamlalo = 0;
             for (int i=0; i<n; i++)
             {
@@ -76,7 +77,55 @@ namespace Jakab_acs_eszter_nezoter_2021._03._22
                     }  
                 }
             }
-            Console.WriteLine($"Az előadásra eddig {szamlalo} jegyet adtak el, ez a nézőtér {szamlalo  /  n * 100}");
+            Console.WriteLine($"Az előadásra eddig {szamlalo} jegyet adtak el, ez a nézőtér {Math.Round((double)szamlalo  /  nezoszam * 100,0)}%-a.");
+
+            //4.feladat
+            int leg = 0;
+            int egy = 0, ketto = 0, harom = 0, negy = 0, ot = 0;
+            for (int i = 0;i<n;i++)
+            {
+                for (int k = 0;k<adatok[i].foglaltsag.Length;k++)
+                {
+                    if (adatok[i].foglaltsag[k] == 'x')
+                    {
+                        if (adatok[i].kategoria[k] == '1')
+                        {
+                            egy++;
+                        }
+                        if (adatok[i].kategoria[k] == '2')
+                        {
+                            ketto++;
+                        }
+                        if (adatok[i].kategoria[k] == '3')
+                        {
+                            harom++;
+                        }
+                        if (adatok[i].kategoria[k] == '4')
+                        {
+                            negy++;
+                        }
+                        if (adatok[i].kategoria[k] == '5')
+                        {
+                            ot++;
+                        }
+                    }
+                }
+
+            }
+            int[] legnagyobb = { egy, ketto, harom, negy, ot };
+            int j = 0;
+            for (int i = 0; i<legnagyobb.Length; i++)
+            {
+                if (legnagyobb[i] > leg)
+                {
+                    leg = legnagyobb[i];
+                    j = i;
+                }
+            }
+            Console.WriteLine($"A legtöbb jegyet a(z) {j+1}. árkategóriában értékesítették.");
+
+            //5.feladat
+
             Console.ReadKey();
         }
     }
